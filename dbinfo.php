b7d0a1d30329853f02fe088230919262b58cce77
root <root@tea42>
2020-06-22 19:57:28 +0200

    Switched from gmaps to openStreetMap, bug fixs and optimisations, css improvement

--- dbinfo.php
+++ dbinfo.php
@@ -1,7 +1,8 @@
 <?php
 $db_host = '127.0.0.1';
 $db_user = 'fail2ban';
-$db_pwd = 'fail2ban';
+$db_pwd = 'glyKS66tNxPPxQFO';
+
 $database = 'fail2ban';
 $table = 'fail2ban';
-
+?>
--- getstats.php
+++ getstats.php
@@ -28,14 +28,23 @@ $xx['totalip'] = getdataset("SELECT COUNT(*) as count FROM $table");
 // Get total number of countries
 $xx['totalcountry'] = getdataset("SELECT COUNT(Distinct country) as count FROM $table");
  
+//number of attack per country
+foreach(getdataset("SELECT code3, country, code, count(id) as count FROM $table group by country") as $c){
+	$xx['totalpercountry'][$c['code3']]=$c;
+}
+
+
 // Get count for each protocol
-$xx['protos'] = getdataset("SELECT name,COUNT(name) as count FROM $table");
+$xx['protos'] = getdataset("SELECT name,COUNT(name) as count FROM $table GROUP BY name");
  
 // Get total counts for each country
-$xx['totals'] = getdataset("SELECT country,COUNT(*) as count FROM $table GROUP BY country ORDER BY count DESC");
+$xx['totals'] = getdataset("SELECT code,country,COUNT(*) as count FROM $table GROUP BY country ORDER BY count DESC LIMIT 5");
  
-//Get last 5 Countires
-$xx['last'] = getdataset("SELECT country,timestamp FROM $table ORDER BY timestamp DESC limit 5");
+//Get last 5 Countries
+$xx['last'] = getdataset("SELECT code,country,MAX(timestamp) FROM $table GROUP BY country ORDER BY timestamp DESC limit 5");
+
+//Get last IPs
+$xx['lastips'] = getdataset("SELECT ip,code,country, timestamp FROM $table ORDER BY timestamp DESC limit 30");
  
 // Return the data in json
 print json_encode($xx);
