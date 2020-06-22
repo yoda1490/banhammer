b4d8498cf26b18a387f5496347a97b8164fc0e8c
root <root@tea42>
2020-06-22 19:57:28 +0200

    Switched from gmaps to openStreetMap, bug fixs and optimisations, css improvement

--- maps.js
+++ maps.js
@@ -1,141 +1,155 @@
-      var map;
+var map;
 
-      function clearOverlays() {
-      for (var i = 0; i < markersArray.length; i++ ) {
-      markersArray[i].setMap(null);
-      }
-        markersArray.length = 0;
-      }
+var stats;
 
-      function updatex()
-      {
-          clearOverlays();
-	  loadmarkers();
-      }
+//used to determine color scale
+var maxPerCountry=0;
+var countryCounter=0;
 
-      function loadmarkers()
-      {
+var markersArray=[];
+function clearOverlays() {
+    for (var i = 0; i < markersArray.length; i++) {
+        markersArray[i].setMap(null);
+    }
+    markersArray.length = 0;
+}
 
+function updatex() {
+    clearOverlays();
+    loadmarkers();
+}
 
- 	$.getJSON("getmarkers.php", function( data ) {
+function loadmarkers() {
+    $.getJSON("getmarkers.php", function(data) {
+        for (var i in data) {
+            createMarker(data[i]);
+        }
+    });
 
-          for (var i in data) {
-            var name = data[i].geo;
-            var address = data[i].ip;
-            var type = data[i].name;
-            var point = new google.maps.LatLng(parseFloat(data[i].latitude),parseFloat(data[i].longitude));
-            createMarker(point, name, address, type);
-	  } 
-	 });
+    $.getJSON("getstats.php", function(data) {
+        stats=data;
+	$('.stat ol').empty();
+        $(data['totals']).each(function(i,j) {
+            $("#top5").append("<li><img src=\"images/flags/" + j.code + ".png\" alt=\"" + j.code + "\" /> " + j.country + " (" + j.count + ")" + "</li>");
+        });
 
-	$.getJSON("getstats.php", function( data ) 
-	{
+        $(data['last']).each(function(i,j) {
+            $("#last5").append("<li title=\""+j.timestamp+"\"><img src=\"images/flags/" + j.code + ".png\" alt=\"" + j.code + "\" /> " + j.country + "</li>");
+        });
 
-		var totals = data['totals'];
-		var countrys = data['last'];
-		var protos = data['protos'];
+        $(data['lastips']).each(function(i,j) {
+            $("#lastips").append("<li title=\""+j.country+"\"><img src=\"images/flags/" + j.code + ".png\" alt=\"" + j.code + "\" /> " + j.timestamp.split(' ')[1] + ' ' + j.ip + "</li>");
+        });
 
-		for (var i in totals)
-		{
-			var country = totals[i].country;
-			var count = totals[i].count;
-			var name = country_code_to_country(country);
-			$("#top5").append("<li><img src=\"images/flags/"+country+".png\" alt=\""+country+"\" title=\"\" /> "+name+" ("+count+")"+"</li>");
-		}
+        $(data['protos']).each(function(i,j) {
+            $("#protocols").append("<li>" + j.name + " (" + j.count + ")</li>");
+        });
 
-		for (var i in countrys)
-		{
-			var country = countrys[i].country;
-			var name = country_code_to_country(country);
-			$("#last5").append("<li><img src=\"images/flags/"+country+".png\" alt=\""+country+"\" title=\"\" /> "+name+"</li>");
-		}
+        countryCounter=data['totalpercountry'];
+        for(let i in countryCounter){
+          if(countryCounter[i].count>maxPerCountry)maxPerCountry=countryCounter[i].count;
+        };
 
-		for (var i in protos)
-		{
-			var name = protos[i].name;
-			var count = protos[i].count;
+        $("#ipsblocked").html(" " + data['totalip'][0].count);
+        $("#countriesclocked").html(" " + data['totalcountry'][0].count);
 
-			$("#protocols").append("<li>"+name+" ("+count+")</li>");
-		}
-	
-		 $("#ipsblocked").html(" "+data['totalip'][0].count);
-		 $("#countriesclocked").html(" "+data['totalcountry'][0].count);
+        L.geoJson(countryData, {style: style}).addTo(map);
 
-	});
+    });
 
 
-      }
+}
 
-      function createMarker(point, name, address, type) {
+function createMarker(data) {
 
-          words = type+" attack from "+address+"\n"+name;
-
-          iname="markers/red_MarkerA.png"
-
-          if(type=="wordpress")
-	  {
-             iname="markers/blue_MarkerA.png"
-	  }
-          if(type=="ssh")
-	  {
-             iname="markers/yellow_MarkerA.png"
-	  }
-          if(type=="dovecot")
-	  {
-             iname="markers/green_MarkerA.png"
-	  }
-          if(type=="sasl")
-	  {
-             iname="markers/brown_MarkerA.png"
-	  }
     
-	
+    color='red';
+    letter='U';
 
-          var marker = new google.maps.Marker({
-                position: point,
-                title:name,
-                icon: iname
-                });
+    if(data.ban == 0) color='paleblue';
 
-          marker.setMap(map);
+    if (data.name.match(/ssh/)) {
+        letter = 'S';
+    }else if (data.name.match(/apache/)) {
+        letter = 'A';
+    }else if (data.name.match(/postfix/)) {
+        letter = 'P';
+    }else if (data.name.match(/sasl/)) {
+        letter = 'S';
+    }
 
-          var html = "<b>" + type+ " ATTACK" + "</b><br />"+name + "<br/>" + address;
+    iname = "markers/"+color+"_Marker"+letter+".png";
 
-          var infowindow = new google.maps.InfoWindow({
-              content: html
-          });
-
-          google.maps.event.addListener(marker, 'click', (function() {
-              infowindow.open(map, marker);
-
- 	      if(oldinfowindow!=null)
-              {
-                   oldinfowindow.close();
-              }
-
-	      oldinfowindow=infowindow;
-         }));
-      }
+    var icon = L.icon({
+        iconUrl: iname,
+        iconAnchor: [10, 34]
+    });
 
 
-      function initialize() {
-          var mapOptions = {
-              center: new google.maps.LatLng(50.448807, -3.746826),
-              zoom: 2
-          };
-          map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+    var html = "<b>" + data.name + " " + "</b><br />" + data.country + ", "+data.city+" <br/>" + data.ip;
 
- 	  loadmarkers();
-      }
+    L.marker({lon: data.longitude, lat: data.latitude}, {icon: icon}).bindPopup(html).addTo(map);
+}
 
-      function myTimer()
-      {
-	  updatex();
-      }
 
-      var oldinfowindow=null;	
+function getColor(d) {
+    if(typeof(countryCounter[d]) != 'undefined'){
+      d=countryCounter[d].count;
+    }else{
+      d=0;
+    }
+    
+    return d > maxPerCountry*90/100 ? '#800026' :
+           d > maxPerCountry*80/100  ? '#BD0026' :
+           d > maxPerCountry*70/100  ? '#E31A1C' :
+           d > maxPerCountry*60 /100 ? '#FC4E2A' :
+           d > maxPerCountry*50/100   ? '#FD8D3C' :
+           d > maxPerCountry*40/100   ? '#FEB24C' :
+           d > maxPerCountry*30/100   ? '#FED976' :
+                      '#FFEDA0';
+}
 
-      google.maps.event.addDomListener(window, 'load', initialize);
+function style(feature) {
 
-      var myVar=setInterval(function(){myTimer()},1000*60*5);
+    return {
+        fillColor: getColor(feature.id),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
 
+var oldinfowindow = null;
+var map;
+
+$(document).ready(function() {
+   // initialize Leaflet
+    //map = L.map('map').setView({lon: 0, lat: 0}, 2);
+
+    map = L.map('map', {
+        center: [0, 0],
+        zoom: 2
+    });
+
+    // add the OpenStreetMap tiles
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      worldCopyJump: true,
+      attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+
+    // show the scale bar on the lower left corner
+    L.control.scale().addTo(map);
+    loadmarkers();
+
+    //need stats
+    //L.geoJson(countryData, {style: style}).addTo(map);
+
+
+    var myVar = setInterval(function() {
+        updatex();
+    }, 1000 * 60 * 5);
+});
