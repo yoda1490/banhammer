e4966c13bcbcaf62300d3ecbf9d67af41c598f5c
root <root@tea42>
2020-06-22 20:49:48 +0200

    Updated MySQL schema

--- fail2sql/fail2ban.sql
+++ fail2sql/fail2ban.sql
@@ -1,19 +1,61 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Table structure for table `fail2ban`
+-- Base de données : `fail2ban`
 --
 
-DROP TABLE IF EXISTS `fail2ban`;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fail2ban`
+--
+
 CREATE TABLE `fail2ban` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `protocol` varchar(4) NOT NULL,
-  `port` int(11) NOT NULL,
-  `ip` varchar(20) NOT NULL,
-  `count` int(11) NOT NULL,
-  `longitude` varchar(20) DEFAULT NULL,
-  `latitude` varchar(20) DEFAULT NULL,
-  `country` varchar(5) DEFAULT NULL,
-  `geo` varchar(255) DEFAULT NULL,
-  `timestamp` DATETIME,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM;
+  `id` int(11) NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `protocol` varchar(4) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ports` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ip` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `longitude` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `latitude` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `code3` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT '',
+  `country` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  `ban` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'is currently ban'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `fail2ban`
+--
+ALTER TABLE `fail2ban`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `fail2ban`
+--
+ALTER TABLE `fail2ban`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
