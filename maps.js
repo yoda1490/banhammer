51a2459eeadf183423219d92d9566d644e494f6d
yoda1490 <506819+yoda1490@users.noreply.github.com>
2025-11-19 23:03:16 +0100

    Optimisation: distribution des couleurs basée sur seuils réels, amélioration des performances et ajout d'un loader

--- maps.js
+++ maps.js
@@ -10,6 +10,7 @@ var stats;
 var maxPerCountry=0;
 var countryCounter=0;
 var colorThresholds=[];
+var isLoading = false;
 
 var markersArray=[];
 function clearOverlays() {
@@ -23,35 +24,63 @@ function clearOverlays() {
         
 }
 
+function showLoader() {
+    if (!isLoading) {
+        isLoading = true;
+        $('#map').append('<div id="map-loader" class="loader-overlay"><div class="loader-spinner"></div><div class="loader-text">Loading data...</div></div>');
+    }
+}
+
+function hideLoader() {
+    isLoading = false;
+    $('#map-loader').fadeOut(300, function() {
+        $(this).remove();
+    });
+}
+
 function updatex() {
     clearOverlays();
     loadmarkers();
 }
 
 function loadmarkers() {
-    $.getJSON("get.php?action=markers", function(data) {
-        for (var i in data) {
-            createMarker(data[i]);
-        }
-    });
+    showLoader();
+    
+    // Charger les stats d'abord (plus rapide)
     $.getJSON("get.php?action=stats", function(data) {
         stats=data;
 	$('.stat ol').empty();
-        $(data['totals']).each(function(i,j) {
-            $("#top5").append("<li><img src=\"images/flags/" + j.code + ".png\" alt=\"" + j.code + "\" /> " + j.country + " (" + j.count + ")" + "</li>");
-        });
-
-        $(data['last']).each(function(i,j) {
-            $("#last5").append("<li title=\""+j.timestamp+"\"><img src=\"images/flags/" + j.code + ".png\" alt=\"" + j.code + "\" /> " + j.country + "</li>");
-        });
-
-        $(data['lastips']).each(function(i,j) {
-            $("#lastips").append("<li title=\""+j.country+" - "+j.timestamp+"\"><img src=\"images/flags/" + j.code + ".png\" alt=\"" + j.code + "\" /> " + j.timestamp.split(' ')[1] + ' <a href="#" onclick="whois(\''+j.id+'\');return false;">' + j.ip + "</a></li>");
-        });
-
-        $(data['protos']).each(function(i,j) {
-            $("#protocols").append("<li>" + j.name + " (" + j.count + ")</li>");
-        });
+        
+        // Optimisation: construire le HTML en une seule fois au lieu de multiples append
+        var top5Html = '';
+        var last5Html = '';
+        var lastipsHtml = '';
+        var protosHtml = '';
+        
+        for(var i = 0; i < data['totals'].length; i++) {
+            var j = data['totals'][i];
+            top5Html += "<li><img src='images/flags/" + j.code + ".png' alt='" + j.code + "' /> " + j.country + " (" + j.count + ")</li>";
+        }
+        
+        for(var i = 0; i < data['last'].length; i++) {
+            var j = data['last'][i];
+            last5Html += "<li title='" + j.timestamp + "'><img src='images/flags/" + j.code + ".png' alt='" + j.code + "' /> " + j.country + "</li>";
+        }
+        
+        for(var i = 0; i < data['lastips'].length; i++) {
+            var j = data['lastips'][i];
+            lastipsHtml += "<li title='" + j.country + " - " + j.timestamp + "'><img src='images/flags/" + j.code + ".png' alt='" + j.code + "' /> " + j.timestamp.split(' ')[1] + " <a href='#' onclick='whois(\"" + j.id + "\");return false;'>" + j.ip + "</a></li>";
+        }
+        
+        for(var i = 0; i < data['protos'].length; i++) {
+            var j = data['protos'][i];
+            protosHtml += "<li>" + j.name + " (" + j.count + ")</li>";
+        }
+        
+        $("#top5").html(top5Html);
+        $("#last5").html(last5Html);
+        $("#lastips").html(lastipsHtml);
+        $("#protocols").html(protosHtml);
 
         countryCounter=data['totalpercountry'];
         
@@ -60,23 +89,23 @@ function loadmarkers() {
         for(let i in countryCounter){
           countryCounts.push(countryCounter[i].count);
           if(countryCounter[i].count>maxPerCountry)maxPerCountry=countryCounter[i].count;
-        };
+        }
         
         // Sort counts in descending order
         countryCounts.sort(function(a, b){return b - a});
         
-        // Define thresholds to ensure top 5 countries get darkest color
-        // and better distribution across the rest
+        // Distribution optimisée basée sur les données réelles:
+        // Top 5: >8500, Top 10: >5000, etc.
         if(countryCounts.length >= 5){
           colorThresholds = [
-            countryCounts[4],      // Top 5 (darkest)
-            countryCounts[Math.min(10, countryCounts.length-1)],  // Top 6-10
-            countryCounts[Math.min(20, countryCounts.length-1)],  // Top 11-20
-            countryCounts[Math.min(35, countryCounts.length-1)],  // Top 21-35
-            countryCounts[Math.min(50, countryCounts.length-1)],  // Top 36-50
-            countryCounts[Math.min(75, countryCounts.length-1)],  // Top 51-75
-            countryCounts[Math.min(100, countryCounts.length-1)], // Top 76-100
-            1  // Any country with at least 1 ban
+            countryCounts[4],      // Top 5 (>8000) - Très rouge foncé
+            1000,                   // >1000 bans - Rouge foncé
+            500,                    // >500 bans - Rouge
+            200,                    // >200 bans - Orange-rouge
+            100,                    // >100 bans - Orange
+            50,                     // >50 bans - Orange clair
+            20,                     // >20 bans - Jaune
+            1                       // >=1 ban - Jaune très clair
           ];
         }else{
           // Fallback for fewer countries
@@ -103,7 +132,19 @@ function loadmarkers() {
         }else{
             colorizedCountryLayer.addData(countryData, {style: style});
         }
+        
+        // Charger les markers après les stats pour éviter le blocage
+        $.getJSON("get.php?action=markers", function(markerData) {
+            for (var i in markerData) {
+                createMarker(markerData[i]);
+            }
+            hideLoader();
+        }).fail(function() {
+            hideLoader();
+        });
 
+    }).fail(function() {
+        hideLoader();
     });
 
 
@@ -186,16 +227,16 @@ function getColor(d) {
       d=0;
     }
     
-    // Use calculated thresholds for better distribution
-    return d >= colorThresholds[0] ? '#800026' :  // Top 5 - Very dark red
-           d >= colorThresholds[1] ? '#BD0026' :  // Top 6-10 - Dark red
-           d >= colorThresholds[2] ? '#E31A1C' :  // Top 11-20 - Red
-           d >= colorThresholds[3] ? '#FC4E2A' :  // Top 21-35 - Orange-red
-           d >= colorThresholds[4] ? '#FD8D3C' :  // Top 36-50 - Orange
-           d >= colorThresholds[5] ? '#FEB24C' :  // Top 51-75 - Light orange
-           d >= colorThresholds[6] ? '#FED976' :  // Top 76-100 - Yellow
-           d >= colorThresholds[7] ? '#FFEDA0' :  // Rest with at least 1 ban - Light yellow
-                      '#FFFFFF';                   // No bans - White
+    // Distribution optimisée avec des seuils réels
+    return d >= colorThresholds[0] ? '#800026' :  // Top 5 (>8500) - Très rouge foncé
+           d >= colorThresholds[1] ? '#BD0026' :  // >1000 bans - Rouge foncé
+           d >= colorThresholds[2] ? '#E31A1C' :  // >500 bans - Rouge
+           d >= colorThresholds[3] ? '#FC4E2A' :  // >200 bans - Orange-rouge
+           d >= colorThresholds[4] ? '#FD8D3C' :  // >100 bans - Orange
+           d >= colorThresholds[5] ? '#FEB24C' :  // >50 bans - Orange clair
+           d >= colorThresholds[6] ? '#FED976' :  // >20 bans - Jaune
+           d >= colorThresholds[7] ? '#FFEDA0' :  // >=1 ban - Jaune très clair
+                      '#FFFFFF';                   // No bans - Blanc
 }
 
 function style(feature) {
